18246de987b068cf6110e9df65d2913e71fa29c7
mohith <mohithkumar808@gmail.com>
2024-01-15 01:10:03 +0530

    gitignore

---
+++ node_modules/next-i18next/dist/es/types.js
@@ -0,0 +1,10 @@
+/* tslint:disable no-explicit-any */
+import { I18nContext, useTranslation, Trans, withTranslation, Translation } from 'react-i18next';
+import { appWithTranslation, i18n } from './';
+
+/**
+ * Inlined from `import('next').NextConfig.i18n` v13.0.6. As we support
+ * multiple nextjs versions it's safer to inline and keep it up-to-date.
+ */
+
+export { i18n, I18nContext, appWithTranslation, useTranslation, Trans, Translation, withTranslation };
