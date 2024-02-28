5f724be0d0ccd56a525b6556d128c91e7b23624e
mohith <mohithkumar808@gmail.com>
2024-02-29 01:54:11 +0530

    select filter bug

--- components/AskDoctors/AskDoctors.js
+++ components/AskDoctors/AskDoctors.js
@@ -63,10 +63,6 @@ function AskDoctors() {
     asyncFetch();
   }, []);
 
-  function handleListItemClick(event, role) {
-    setSelectedIndex(role);
-  }
-
   // Extract unique roles from doctorsData
   const uniqueRoles = [
     "All",
