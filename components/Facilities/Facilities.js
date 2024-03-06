ff374ee18bb1a758263501da3d59bc7ada740738
mohith <mohithkumar808@gmail.com>
2024-03-07 02:00:11 +0530

    facilities sscell

--- components/Facilities/Facilities.js
+++ components/Facilities/Facilities.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
 import Container from "@mui/material/Container";
@@ -7,64 +7,12 @@ import Carousel from "react-slick";
 import PrevIcon from "@mui/icons-material/ArrowBack";
 import NextIcon from "@mui/icons-material/ArrowForward";
 import { useTranslation } from "next-i18next";
+import Papa from "papaparse";
 import imgApi from "~/public/images/imgAPI";
 import useStyles from "./facilities-style";
 import TitleIcon from "../Title/WithIcon";
 import Card from "../Cards/Default";
 import DotsParallax from "../Parallax/Dots";
-
-const facilityList = [
-  // {
-  //   title: "B.SC NURSING AND DMLT COURSES AVAILABLE",
-  //   desc: "Proin ac arcu nisl. Duis eu molestie lectus. Nam quis mauris faucibus, aliquet elit eu, rhoncus ipsum.",
-  //   img: imgApi.medical[3],
-  // },
-  {
-    title: "Pharmacy",
-    desc: "he hospital has got 24*7 Pharmacy with medicines and medical apparatus of trusted companies and skilled pharamacist.",
-    img: imgApi.medical[6],
-  },
-  {
-    title: "Canteen",
-    desc: "There is a Canteen within the hospital providing home like food and other daily used items. It prepares food for the patient according to the specially prescribed by the doctors.",
-    img: imgApi.medical[6],
-  },
-  {
-    title: "Parking",
-    desc: "The hospital has got good parking facility with guidance of security staff",
-    img: imgApi.medical[6],
-  },
-  {
-    title: "FIRE FIGHTING SYSTEM",
-    desc: "The hospital has got well equipped fire fighting system  and advanced machines which may be a live saver during emergency.",
-    img: imgApi.medical[6],
-  },
-  {
-    title: "PROFICIENT NURSE",
-    desc: "The hospital has got Nursing, technical, laboratory staff  with various fields such as B.Sc Nursing, ANM, GNM, DMLT etc with a good experience and fantastic skill.",
-    img: imgApi.medical[5],
-  },
-  {
-    title: "TRAINED SECURITY",
-    desc: "The hospital has got two different types of security from agency and of it's own.",
-    img: imgApi.medical[4],
-  },
-  {
-    title: "DENTAL FACILITY",
-    desc: "The hospital is specialised in dental facility with specialised machinery, experienced Dentist, supporting staff etc.",
-    img: imgApi.medical[3],
-  },
-  {
-    title: "CCTV SURVEILLENCE",
-    desc: "The hospital has 24*7 CCTV surveillance with high definition recording & clear night vision present everywhere in the hospital",
-    img: imgApi.medical[2],
-  },
-  {
-    title: "PHYSIOTHERAPY",
-    desc: "The hospital have got a Physiotherapy department with various specialised equipments.",
-    img: imgApi.medical[2],
-  },
-];
 
 function Facilities() {
   const { t } = useTranslation("common");
@@ -72,6 +20,7 @@ function Facilities() {
   const isDesktop = useMediaQuery(theme.breakpoints.up("lg"));
   const { classes } = useStyles();
   const slider = useRef(null);
+  const [facilityList, setFacilityList] = useState([]);
   const settings = {
     dots: false,
     infinite: false,
@@ -103,11 +52,28 @@ function Facilities() {
   };
 
   useEffect(() => {
-    if (theme.direction === "ltr" && window.innerWidth > 1200) {
-      const limit = window.innerWidth > 1400 ? 3 : 2;
-      const lastSlide = Math.floor(facilityList.length - limit);
-      slider.current.slickGoTo(lastSlide);
-    }
+    const asyncFetch = async () => {
+      const csvUrl =
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vRuv0meN32gExglqFtY_Sob3NEgqIw-uuORdLuxbCWiBt-BpIRSrb8h4Y5yb4IDZaYJjnNnA7L-_R_R/pub?output=csv";
+      try {
+        const response = await fetch(csvUrl);
+        const text = await response.text();
+        Papa.parse(text, {
+          header: true,
+          dynamicTyping: true,
+          complete: (result) => {
+            setFacilityList(result.data);
+          },
+          error: (error) => {
+            console.error("Error parsing CSV:", error);
+          },
+        });
+      } catch (error) {
+        console.error("Error fetching CSV data:", error);
+      }
+    };
+
+    asyncFetch();
   }, []);
 
   return (
@@ -128,7 +94,7 @@ function Facilities() {
                 <Card
                   title={item.title}
                   desc={item.desc}
-                  img={item.img}
+                  img={item.img} // Assuming you have 'img' field in CSV
                   button={t("medical-landing.services_button")}
                 />
               </div>
